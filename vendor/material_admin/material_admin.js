a290e596f31bd80b86756822de107ad7b1d29062
Tracy Cai <tracy.cai@openapply.com>
2021-01-13 16:23:37 +0800

    misc - Change directory name

--- vendor/material_admin/material_admin.js
+++
@@ -1,8 +0,0 @@
-import '../material_admin/js/jquery.min';
-import '../material_admin/js/bootstrap.min';
-
-import '../material_admin/js/sticky-kit.min.js';
-import '../material_admin/js/jquery.slimscroll';
-import '../material_admin/js/custom';
-import '../material_admin/js/dashboard';
-import '../material_admin/js/sidebarmenu';
---
+++ vendor/rails_material_admin/material_admin.js
@@ -0,0 +1,8 @@
+import './js/jquery.min';
+import './js/bootstrap.min';
+
+import './js/sticky-kit.min.js';
+import './js/jquery.slimscroll';
+import './js/custom';
+import './js/dashboard';
+import './js/sidebarmenu';
