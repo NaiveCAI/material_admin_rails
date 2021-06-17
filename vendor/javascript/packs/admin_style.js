8f807d5facad9e75936359cd30f862fe51fd7b89
Tracy Cai <tracy.cai@openapply.com>
2021-06-17 11:22:38 +0800

    bug - Fix css manifest file

--- vendor/javascript/packs/admin_style.js
+++ vendor/javascript/packs/admin_style.js
@@ -1,5 +1,5 @@
 import '../../../node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css';
-import '../../rails_material_admin/material_admin.scss';
+import '../vendor/rails_material_admin/material_admin.scss';
 import '../src/stylesheets/shared/atom.scss';
 import '../src/stylesheets/shared/override.scss';
 import '../src/stylesheets/admin/shared/common.scss';
